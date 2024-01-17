1456acc41f69af0c3a8dc1da1569b98fba491ae9
John <johnvazna@gmail.com>
2024-01-17 09:18:36 -0600

    Add new endpoint for artist details retrieval by id

--- routes/artists.js
+++ routes/artists.js
@@ -22,4 +22,27 @@ router.get('/', authenticateToken, async (req, res) => {
     }
 });
 
+router.get('/:id', authenticateToken, async (req, res) => {
+    try {
+        const artistId = req.params.id;
+        const query = `
+            SELECT u.userId, u.userType, u.name, u.lastname, u.email, 
+                   a.semblance, a.art_style, a.birthday, a.origin, a.sex, a.city
+            FROM users u
+            JOIN artists a ON u.userId = a.userId
+            WHERE u.userType = 'artist' AND u.userId = ?`;
+
+        const [artist] = await database.query(query, [artistId]);
+
+        if (artist.length === 0) {
+            return res.status(404).send('Artist not found');
+        }
+
+        res.json(artist[0]);
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Internal server error');
+    }
+});
+
 module.exports = router;
